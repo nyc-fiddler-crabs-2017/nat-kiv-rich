084d9a1f10ca3308b6a8e9d413c6ca6c79794c4d
Kiv Lo & Richard Kim <pair+kivyy.richardykim@devbootcamp.com>
2017-05-12 14:41:20 -0400

    Add login and register pages

---
+++ app/controllers/login.rb
@@ -0,0 +1,20 @@
+get '/login' do
+  erb :'user_settings/login'
+end
+
+post '/login' do
+  @user = User.find_by(username: params[:user][:username])
+  if @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    redirect '/login'
+  end
+
+end
+
+
+get '/logout' do
+  session.clear
+end
+
